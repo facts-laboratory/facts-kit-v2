f21cc9fdff830feb71c609dc3828bf2c220f1c44
Justin Shaw <mogulx_operates@yahoo.com>
2023-07-10 17:24:13 -0500

    feat: buy

--- packages/facts-sdk/src/api/buy.js
+++ packages/facts-sdk/src/api/buy.js
@@ -1,3 +1,68 @@
-export function buy() {
-  return async () => {};
+import Async, { fromPromise } from '../common/hyper-async.js';
+import { allowU } from './allow.js';
+import { getPrice } from './get-price.js';
+
+/**
+ * @author @jshaw-ar
+ * @export
+ * @param {{warp: any, signer: any}} input
+ */
+export function buy({ warp, signer }) {
+  return async ({ qty, tx, allowTx, position }) => {
+    return Async.of({ qty, tx, allowTx, position, warp })
+      .chain(fromPromise(getPriceWrapper))
+      .chain(({ result, dre }) =>
+        fromPromise(allowU)(tx, result, dre, warp, signer)
+      )
+      .chain(({ result, allowTx, dre }) =>
+        fromPromise(factMarketPosition)(tx, allowTx, result, dre, warp, signer)
+      )
+      .fork(
+        (/** @type {{ message: any; }} */ error) => {
+          throw new Error(error?.message || error || 'An error occurred');
+        },
+        (/** @type {any} */ output) => output
+      );
+  };
 }
+
+/**
+ *
+ * @author @jshaw-ar
+ * @param {{qty: number, tx: string, position: 'support' | 'oppose', warp: any}} input
+ * @return {Promise<any>}
+ */
+const getPriceWrapper = async ({ qty, tx, position, warp }) => {
+  return getPrice(warp)({ qty, tx, position });
+};
+
+/**
+ *
+ * @author @jshaw-ar
+ * @param {string} tx
+ * @param {string} allowTx
+ * @param {any} result
+ * @param {string} dre
+ * @param {any} warp
+ * @param {any} signer
+ * @return {Promise<{tx: string, result: any, dre: string}>}
+ */
+const factMarketPosition = async (tx, allowTx, result, dre, warp, signer) => {
+  const interaction = await warp
+    .contract(tx)
+    .setEvaluationOptions({
+      remoteStateSyncSource: `https://${dre}.warp.cc/contract`,
+      remoteStateSyncEnabled: true,
+      internalWrites: true,
+      allowBigInt: true,
+      unsafeClient: 'skip',
+    })
+    .connect(signer)
+    .writeInteraction({
+      function: 'buy',
+      ...result,
+      tx: allowTx,
+    });
+
+  return { tx: interaction.originalTxId, result, dre };
+};
--- packages/facts-sdk/test/buy.test.js
+++ packages/facts-sdk/test/buy.test.js
@@ -1,10 +1,36 @@
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
+import * as fs from 'fs';
+
+import { buy } from '../src/api/buy.js';
+import { getWarp } from '../test-tools/warp.js';
 
 const test = suite('buy');
 
-test('buy', () => {
-  assert.is('buy', 'buy');
+const jwk = JSON.parse(
+  fs.readFileSync(process.env['PATH_TO_WALLET']).toString()
+);
+
+test('buy', async () => {
+  const warp = getWarp();
+  await buy({ warp, signer: jwk })({
+    qty: 1,
+    tx: 'OwDhifPxKBKK1ArPMtjRrg4DjW3XcOpw0q3gBLWu3dY',
+    position: 'support',
+  });
+  const read = await warp
+    .contract('OwDhifPxKBKK1ArPMtjRrg4DjW3XcOpw0q3gBLWu3dY')
+    .setEvaluationOptions({
+      remoteStateSyncSource: `https://dre-3.warp.cc/contract`,
+      remoteStateSyncEnabled: true,
+      internalWrites: true,
+      allowBigInt: true,
+      unsafeClient: 'skip',
+    })
+    .readState();
+
+  const state = await read.cachedValue.state;
+  assert.is(Number.isInteger(state.creator_cut), true);
 });
 
 test.run();
