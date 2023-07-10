250f630f880a78df6af67ece2e7d5dc72bf5fda5
Justin Shaw <mogulx_operates@yahoo.com>
2023-07-10 17:24:52 -0500

    refactor: export all funcs out of api

--- packages/facts-sdk/src/main.js
+++ packages/facts-sdk/src/main.js
@@ -1,3 +1,9 @@
-import { getPrice } from './api/get-price';
+import { getPrice } from './api/get-price.js';
+import { getBalance } from './api/get-balance.js';
+import { getSupply } from './api/get-supply.js';
+import { buy } from './api/buy.js';
+import { sell } from './api/sell.js';
+import { attach } from './api/attach-fm.js';
+import { deploy } from './api/deploy-fm.js';
 
-export { getPrice };
+export { getPrice, getBalance, getSupply, buy, sell, attach, deploy };
