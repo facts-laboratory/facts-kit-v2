99cfede693da77ab0e1df02a29e6306f6d519d2a
Justin Shaw <mogulx_operates@yahoo.com>
2023-07-12 10:48:58 -0500

    refactor: export all new features

--- packages/facts-sdk/src/main.js
+++ packages/facts-sdk/src/main.js
@@ -5,5 +5,39 @@ import { buy } from './api/buy.js';
 import { sell } from './api/sell.js';
 import { attach } from './api/attach-fm.js';
 import { deploy } from './api/deploy-fm.js';
+import { getSellPrice } from './api/get-sell-price.js';
+import { getById } from './api/query/get-by-id.js';
+import { getByAns110Type as getByType } from './api/query/get-by-type.js';
+import { getFactPositions } from './api/query/get-fact-positions.js';
+import { getFactRebuttals } from './api/query/get-fact-rebuttals.js';
+import { getFeed } from './api/query/get-feed.js';
+import { getPlayerFacts } from './api/query/get-player-facts.js';
+import { getPlayerPositions } from './api/query/get-player-positions.js';
+import { getPlayerRebuttals } from './api/query/get-player-rebuttals.js';
+import { getPlayerRebuttalsIds } from './api/query/get-player-rebuttal-ids.js';
+import { getRenderForRenderers as getRenderers } from './api/query/get-renderers.js';
 
-export { getPrice, getBalance, getSupply, buy, sell, attach, deploy };
+const query = {
+  getById,
+  getByType,
+  getFactPositions,
+  getFactRebuttals,
+  getFeed,
+  getPlayerFacts,
+  getPlayerPositions,
+  getPlayerRebuttals,
+  getPlayerRebuttalsIds,
+  getRenderers,
+};
+
+export {
+  getPrice,
+  getBalance,
+  getSupply,
+  buy,
+  sell,
+  attach,
+  deploy,
+  query,
+  getSellPrice,
+};
