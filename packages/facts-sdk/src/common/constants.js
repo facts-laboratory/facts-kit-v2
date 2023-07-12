221b6a94b50c92931c7e67c83743a79d9a6bd0bf
Justin Shaw <mogulx_operates@yahoo.com>
2023-07-12 10:49:59 -0500

    fix: touchup docs; use Facts-Protocol instead of Permafacts

--- packages/facts-sdk/src/common/constants.js
+++ packages/facts-sdk/src/common/constants.js
@@ -2,7 +2,7 @@ export const U_TX = 'KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw';
 export const FACT_MARKET_SRC = 'eIAyBgHH-H7Qzw9fj7Austj30QKPQn27eaakvpOUSR8';
 
 /**
- * @param {import("../api/deploy-fm").PermafactsTags} input
+ * @param {import("../api/deploy-fm").FactMarketTags} input
  * @return {Array<{name: string;value: string;}>} tags
  */
 export const getAns110TagsFromUser = (input) => {
@@ -29,13 +29,13 @@ export const getAns110TagsFromUser = (input) => {
  * @author @jshaw-ar
  * @export
  * @param {string} [rebutTx]
- * @return {Array.<{name: string, value: string}>} tags
+ * @return {Array.<{name: string; value: string;}>} tags
  */
 export const getFactsTags = (rebutTx) => {
   const factRebuts = rebutTx ? [{ name: 'Fact-Rebuts', value: rebutTx }] : [];
   return [
     {
-      name: 'Permafacts-Type',
+      name: 'Facts-Protocol-Type',
       value: rebutTx ? 'Rebuttal' : 'Assertion',
     },
     { name: 'Facts-Protocol-Version', value: '0.1.0' },
