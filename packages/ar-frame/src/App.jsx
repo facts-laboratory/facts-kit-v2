afeb96aaeb07eb56aa7075dc6d1e547de784e979
Justin Shaw <mogulx_operates@yahoo.com>
2023-07-27 18:33:14 -0500

    feat: ar-frame

---
+++ packages/ar-frame/src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview A simple React component.
+ * @module App
+ */
+
+import React from 'react';
+
+/**
+ * A functional component representing the main application.
+ * @component
+ * @returns {JSX.Element} JSX representation of the component.
+ */
+export const App = () => {
+  return <div>Hello, esbuild with JSX!</div>;
+};
+
+export default App;
