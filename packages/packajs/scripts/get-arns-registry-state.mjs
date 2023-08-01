1cedf3f2ee4263c9b6489fcc23b94072bee8dde0
Justin Shaw <mogulx_operates@yahoo.com>
2023-08-01 09:05:40 -0500

    feat: get-arns-registry script

---
+++ packages/packajs/scripts/get-arns-registry-state.mjs
@@ -0,0 +1,14 @@
+const ARNS_REGISTRY = 'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U';
+
+const getArnsRegistryState = async () => {
+  const response = await fetch(
+    `https://dre-3.warp.cc/contract/?id=${ARNS_REGISTRY}`
+  );
+  if (!response.ok) {
+    throw new Error('Error fetching ARNS Registry state.');
+  }
+
+  return response.json();
+};
+
+getArnsRegistryState().then(console.log).catch(console.log);
