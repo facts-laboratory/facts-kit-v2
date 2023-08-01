564dfedf3b43ba9a3959c905be45925f8f687d53
Justin Shaw <mogulx_operates@yahoo.com>
2023-08-01 07:33:18 -0500

    refactor: move constants to a constants file

---
+++ packages/packajs/src/common/constants.js
@@ -0,0 +1,3 @@
+export const ARNS_REGISTRY = 'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U'; // This is the production ArNS Registry Smartweave Contract
+export const ANT_SOURCE = 'PEI1efYrsX08HUwvc6y-h6TSpsNlo2r6_fWL2_GdwhY'; // This is the ANT Smartweave Contract TX ID
+export const nameToBuy = 'test-domain-packajs'; // This is the name we want to buy from ArNS
