2c507b83e1434940b62757bd382a3030a8fc6ca9
mrsaifullah52 <mrsaifullah52@gmail.com>
2023-08-02 05:28:41 +0500

    feat: arns funcs
    validateInput, validateArns, checkArnsExistence, registerArns

--- packages/packajs/src/api/arns.js
+++ packages/packajs/src/api/arns.js
@@ -3,3 +3,100 @@
  * 1. Check if arns is available if the --available is passed as an option
  * 2. register the arns name if --register is passed as an option.
  */
+
+import chalk from 'chalk';
+import Async, { fromPromise } from '../common/hyper-async.js';
+import { getArnsTags, hasOptions, hasWallet, validateArns } from '../common/util.js';
+import fetch from 'node-fetch';
+import NodeBundlr from '@bundlr-network/client/build/esm/node/bundlr';
+const { ARNS_REGISTRY, DRE_URL, ANT_SOURCE } = require("../common/constants");
+
+
+
+/**
+ * @typedef {Object} PublishOptions
+ * @property {string} arns - The ARNs (Amazon Resource Names) associated with the file.
+ * @typedef {NodeBundlr} bundlr
+ */
+
+
+
+/**
+ *
+ * @param {string} name The name of the ARNS
+ * @param {NodeBundlr} bundlr The bundlr instance.
+ */
+export function ArNs(name, bundlr) {
+    /**
+    * check if the arns name is available then register it. 
+    * @param {PublishOptions} options The options for publishing the JavaScript file. 
+    */
+    return async (options) =>
+        Async.of(options)
+            .chain(fromPromise(validateInput))
+            .chain((/** @type {PublishOptions} */ options) =>
+                fromPromise(checkArnsExistence)(name, options)
+            )
+            .chain((/** @type {PublishOptions} */ options) =>
+                // TODO: have to manage to get the tx id
+                fromPromise(registerArns)(name, bundlr, "kSGwKjy0hwpanOFEFweWw5MLZ1zCgLx4q1JaEUKk7DY", options)
+            )
+            .fork(
+                (error) => {
+                    console.error(
+                        chalk.red(error?.message || error || 'An error occurred.')
+                    );
+                    process.exit();
+                },
+                (output) => {
+                    console.log("output", output);
+                    return output;
+                }
+            );
+}
+
+
+const validateInput = async (options) => {
+    return Async.of(options)
+        .chain(hasOptions)
+        .chain(hasWallet)
+        .chain(fromPromise(validateArns))
+        .toPromise();
+};
+
+/**
+ * @param {string} name The name of the ARNS to check.
+ * @param {PublishOptions} options The options for publishing the JavaScript file.
+ */
+const checkArnsExistence = async (name, options) => {
+    const response = await fetch(
+        `${DRE_URL}/contract/?id=${ARNS_REGISTRY}`
+    );
+    if (!response.ok) {
+        throw new Error('Error fetching ARNS Registry state.');
+    }
+    const results = await response.json();
+
+    if (results.state.records[name]) {
+        throw new Error("ArNS name already exists");
+    }
+
+    console.log("Available to register:- ", name)
+
+    return options;
+};
+
+/**
+ * @param {string} name The name of the ARNS to register.
+ * @param {NodeBundlr} bundlr The bundlr instance.
+ * @param {PublishOptions} options The options for publishing the JavaScript file.
+ * @param {string} tx The transaction id of the file.
+ */
+
+const registerArns = async (name, bundlr, tx, options) => {
+    const tags = getArnsTags(ANT_SOURCE, tx);
+    const response = await bundlr.upload(name, { tags });
+    console.log(`ArNs Registered ==> https://arweave.net/${response.id}`);
+
+    return options;
+}
--- packages/packajs/src/common/constants.js
+++ packages/packajs/src/common/constants.js
@@ -1,3 +1,5 @@
 export const ARNS_REGISTRY = 'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U'; // This is the production ArNS Registry Smartweave Contract
 export const ANT_SOURCE = 'PEI1efYrsX08HUwvc6y-h6TSpsNlo2r6_fWL2_GdwhY'; // This is the ANT Smartweave Contract TX ID
 export const nameToBuy = 'test-domain-packajs'; // This is the name we want to buy from ArNS
+export const MAX_ARNS_NAME_LENGTH = 32; // This is the maximum length of an ArNS name
+export const DRE_URL = 'https://dre-3.warp.cc';  
