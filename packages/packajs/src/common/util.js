77a98c2df4e75bd6fb2f6d3fd2de74de550f38c3
Justin Shaw <mogulx_operates@yahoo.com>
2023-08-03 09:36:33 -0500

    feat: mkdir / readfile

--- packages/packajs/src/common/util.js
+++ packages/packajs/src/common/util.js
@@ -1,9 +1,11 @@
+import chalk from 'chalk';
 import { promises } from 'fs';
+import * as path from 'path';
 
-async function readFileExample() {
+export async function readFile() {
   try {
     // Provide the file path as the first argument to readFile
-    const filePath = './';
+    const filePath = './.packajs/manifest.json';
 
     // The `readFile` method returns a promise, so we can use `await` to wait for the result.
     const fileContent = await promises.readFile(filePath, {
@@ -11,9 +13,31 @@ async function readFileExample() {
     });
 
     // Do something with the file content
-    console.log(fileContent);
+    console.log('CONTENTS', fileContent);
   } catch (error) {
     // Handle any errors that might occur during file reading
     console.error('Error reading the file:', error.message);
+  }
+}
+
+export async function mkdir() {
+  try {
+    const newDirectoryName = '.packajs';
+
+    // Provide the desired directory path as the first argument to mkdir
+    const newDirectoryPath = path.join(__dirname, newDirectoryName);
+
+    // The `mkdir` method returns a promise, so we can use `await` to wait for the result.
+    await promises.mkdir(newDirectoryPath);
+
+    console.log('Directory created successfully!');
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      console.log('EXISTS!');
+      return;
+    } else {
+      console.log(chalk.red(error?.message || 'An error occurred.'));
+      process.exit();
+    }
   }
 }
