758a5db4ca7129c0b81873599f2785de38e48758
Justin Shaw <mogulx_operates@yahoo.com>
2023-11-08 15:20:57 -0600

    ci: simplify build script

--- packages/packajs/scripts/build.js
+++ packages/packajs/scripts/build.js
@@ -1,50 +1,23 @@
-import { build } from 'esbuild';
-import { readFileSync } from 'fs';
-import { extname, dirname as _dirname } from 'path';
-import replace from 'replace-in-file';
-
-const nodeModules = new RegExp(
-  /^(?:.*[\\/])?node_modules(?:\/(?!postgres-migrations).*)?$/
-);
-
-const plugin = {
-  name: 'plugin',
-  setup(build) {
-    build.onLoad({ filter: /.*/ }, ({ path: filePath }) => {
-      if (!filePath.match(nodeModules)) {
-        let contents = readFileSync(filePath, 'utf8');
-        const loader = extname(filePath).substring(1);
-        const dirname = _dirname(filePath);
-        contents = contents
-          .replaceAll('__dirname', `"${dirname}"`)
-          .replaceAll('__filename', `"${filePath}"`);
-        return {
-          contents,
-          loader,
-        };
-      }
-    });
-  },
-};
+import { build } from "esbuild";
+import replace from "replace-in-file";
 
 build({
-  entryPoints: ['./src/cli.js'],
-  outdir: './dist',
-  format: 'esm',
+  entryPoints: ["./src/cli.js"],
+  outdir: "./dist",
+  format: "esm",
   bundle: true,
-  platform: 'node',
+  platform: "node",
   banner: {
-    js: `import { createRequire } from 'module';const require = createRequire(import.meta.url);`,
+    js: `import { createRequire } from 'module';const require = createRequire(import.meta.url);import { fileURLToPath } from 'url';import { dirname } from 'path';const __filename = fileURLToPath(import.meta.url);const __dirname = dirname(__filename);`,
   },
-  plugins: [plugin],
 })
   .then(() =>
     replace.sync({
-      files: './dist/cli.js',
+      files: "./dist/cli.js",
       from: [
         `console.warn("bigint: Failed to load bindings, pure JS will be used (try npm run rebuild?)");`,
       ],
-      to: '',
+      to: "",
       countMatches: true,
     })
   )
@@ -52,4 +25,4 @@ build({
     console.log(e);
     return process.exit(1);
   })
-  .finally(() => console.log('Success.'));
+  .finally(() => console.log("Success."));
